89ebeb61a729ee8c62a61d74e2c60a9f2c2288fa
kaarel <kaarel.kattai@icloud.ee>
2025-05-25 22:19:29 +0300

    Added stickyNote

--- src/main/java/com/kaarel/stickynotes/stickynotesapp/Main.java
+++ src/main/java/com/kaarel/stickynotes/stickynotesapp/Main.java
@@ -1,23 +1,17 @@
 package com.kaarel.stickynotes.stickynotesapp;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
     @Override
-    public void start(Stage stage) {
-        Label label = new Label("Tere, Sticky Note!");
-        Scene scene = new Scene(label, 300, 200);
-        stage.setTitle("StickyNote");
-        stage.setScene(scene);
-        stage.setAlwaysOnTop(true);
-        stage.show();
+    public void start(Stage primaryStage) {
+        StickyNote note = new StickyNote();
+        note.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
